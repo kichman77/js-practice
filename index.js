8cab215fcc846a83c108474b3d5d500bfad0f00c
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-03 17:12:47 +0300

    практика

--- index.js
+++ index.js
@@ -20,3 +20,27 @@
 // let b = '1';
 // let c = a + Number(b); 
 // console.log(c);
+
+// const name = 'Igor';
+// let age = prompt ('введите ваш возраст');
+// console.log(typeof age);
+// // age + age;
+// console.log(typeof age);
+// age = Number(age)
+// const message = 'hello '  + name;
+// console.log(message);
+
+// const newMessage = `my age is ${age}`
+// console.log(newMessage);
+// age = 30;
+// console.log(typeof age);
+
+// let answer = confirm('вам есть 18');
+// console.log(answer);
+// console.log(typeof answer);
+
+// let newquestion = `вы любите js?`;
+// let newAnswer =confirm (newquestion);
+
+
+
