c454815c436cdbff931d57ec23417ba55ecc1040
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-03 15:11:27 +0300

    добавил стартовые файлы

---
+++ index.js
@@ -0,0 +1,22 @@
+// 'use strict'
+// let a;
+// console.log(a);
+
+// let b = 1;
+// b = 2;
+// console.log(b);
+
+// const c = [1, 2];
+// console.log(c);
+// c.push(3);
+// console.log(c);
+
+// a = 1;
+// b = 2;
+// console.log(a + b);
+// alert("hello world");
+
+// let a = 24;
+// let b = '1';
+// let c = a + Number(b); 
+// console.log(c);
