38e5657cb9ed2238b977065b29a2c4ba9b70a851
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-04 13:22:07 +0300

    практика(много вопросов)

--- index.js
+++ index.js
@@ -42,38 +42,111 @@
 // let newquestion = `вы любите js?`;
 // let newAnswer =confirm (newquestion);
 
-let num = 10;
-let boolNum = Boolean(num);
-console.log(boolNum);
-num = 0;
-boolNum = Boolean(num);
-console.log(boolNum);
-let str = `Igor`;
-let boolstr = Boolean(str); 
-console.log(boolstr);
+// let num = 10;
+// let boolNum = Boolean(num);
+// console.log(boolNum);
+// num = 0;
+// boolNum = Boolean(num);
+// console.log(boolNum);
+// let str = `Igor`;
+// let boolstr = Boolean(str); 
+// console.log(boolstr);
 
-str = ``;
-boolstr = Boolean(str);
-console.log(boolstr);
-str = ` `;
-boolstr = Boolean(str);
-console.log(boolstr);
+// str = ``;
+// boolstr = Boolean(str);
+// console.log(boolstr);
+// str = ` `;
+// boolstr = Boolean(str);
+// console.log(boolstr);
 
-let isOpen;
-console.log(isOpen);
-console.log(typeof isOpen);
+// let isOpen;
+// console.log(isOpen);
+// console.log(typeof isOpen);
 
-let boolUnd = Boolean(isOpen);
-console.log(boolUnd);
-console.log(typeof boolUnd);
+// let boolUnd = Boolean(isOpen);
+// console.log(boolUnd);
+// console.log(typeof boolUnd);
 
-isOpen = null;
-console.log(isOpen);
-console.log(typeof isOpen);
+// isOpen = null;
+// console.log(isOpen);
+// console.log(typeof isOpen);
 
-let boolNull = Boolean(isOpen);
-console.log(boolNull);
-console.log(typeof boolNull);
+// let boolNull = Boolean(isOpen);
+// console.log(boolNull);
+// console.log(typeof boolNull);
+
+// =========== 04.09.======================
+
+// ======= сравнение если / то 
+// let vova = {
+//   age : 16,
+//   dad: 'mehanic'
+// }
+// if(vova.age >= 18) {
+//   alert(`Prohodi paren`);
+// }else if (vova.dad === 'deputat'){ 
+//   alert(`Prohodi paren`);
+
+// }else {
+//   alert('Izvini brat');
+// }
+
+// vova.age >= 18 || vova.dad === `deputat`
+// ? alert('Prohodi paren')
+// : alert('Izvini brat'); 
 
 
+// ========= кривой пример/ требует разбора=======
+// const PASSWORD = 'iLovejs'
+// let promt1 = promt('Enter your name: ');
+
+// if(promt1) {
+//   promt === PASSWORD
+//   ? console.log('You"re in')
+//   : console.log('Wrong password!!!)
+// };
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// if (true) {
+//   c = 4;
+// }
+// console.log(c);
+
+// =========== циклы =================
+
+// let q;
+
+// while (q !== null && q !== '') {
+//   q = prompt ( 'enter youre number: ');
+//   console.log(q);
+// }
+
+// =============== for =================
+// for(let i = 0; i<10; i++) {
+//   console.log(i);
+// }
+// for (let b = 0; b<10;) {
+//   console.log(++b);
+// }
+
+// let ageIgor = 39;
+// let ageOleg = 27;
+// const ageTotal = ageIgor + ageOleg; 
+// console.log(ageTotal);
+
+
+let radius = 10;
+let PI = 3.14; 
+const S = PI *(Math.pow(radius, 2 ));
+console.log(S);
+
+
+
+// let name = 'Igor';
+// let age = 39;
+// const group = 24;
+// console.log(`Я ${name}, мне ${age} лет, я учусь на ${group} буткемпе!!!`)
 
