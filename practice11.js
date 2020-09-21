5eea6aff555bfa85cf5f7e4261eb7d7c7deb4d52
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-21 17:35:29 +0300

    praktice Dom

---
+++ practice11.js
@@ -0,0 +1,50 @@
+//  const body = document.querySelector("body");
+// // console.log(body);
+// for (let index = 0; index < 3; index++) {
+//   const button = document.createElement("button");
+//   button.classList = 'btn';
+//   button.textContent = "tap here";
+//   button.style.width = "70px";
+//   button.style.height = "70px";
+//   button.style.margin = "10px";
+//   button.style.backgroundColor = "white";
+//   body.style.background = "teal";
+//   body.append(button);
+
+//   // const buttonList = document.querySelectorAll('btn');
+//   // const btn1 = buttonList[0];
+//   // const btn1 = buttonList[1];
+//   // const btn1 = buttonList[2];
+
+// // не работающий пример!!! необходимо закончить !!!!
+//   button.addEventListener("click", () => {
+//     const button = document.querySelector("button");
+//     body.style.background = "green";
+//   });
+// }
+// --------------------------------------------------
+const body = document.querySelector("body");
+
+
+for (let i = 0; i < 10; i++) {
+  const cube = document.createElement("div");
+  cube.classList.add("cube");
+  cube.textContent = i;
+  body.append(cube);
+
+  cube.addEventListener("click", () => {
+    alert(cube.textContent);
+  });
+}
+
+// window.addEventListener('scroll', () => {
+//   // console.log(window.scrollY);
+//   window.scrollY >= 200 && window.scrollY < 210 ? (body.style.background = 'red') :
+//     window.scrollY > 300 && window.scrollY < 310 ?
+//       (body.style.background = 'blue')
+//       : '';
+// });
+
+body.addEventListener('click', (e) => {
+  body.innerHTML += `<div class='circle' style="top:${e.clientY}px; left:${e.clientX}px"></div>`
+})
